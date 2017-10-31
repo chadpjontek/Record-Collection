2b3f111b2d771f85a14f4abfd06a981d1d840a8a
Chad Pjontek <chadpjontek@gmail.com>
2017-10-31 13:06:08 -0400

    finished algorithm

--- Record-Collection.js
+++ Record-Collection.js
@@ -3,22 +3,22 @@ var collection = {
   "2548": {
     "album": "Slippery When Wet",
     "artist": "Bon Jovi",
-    "tracks": [ 
-      "Let It Rock", 
-      "You Give Love a Bad Name" 
+    "tracks": [
+      "Let It Rock",
+      "You Give Love a Bad Name"
     ]
   },
   "2468": {
     "album": "1999",
     "artist": "Prince",
-    "tracks": [ 
-      "1999", 
-      "Little Red Corvette" 
+    "tracks": [
+      "1999",
+      "Little Red Corvette"
     ]
   },
   "1245": {
     "artist": "Robert Palmer",
-    "tracks": [ ]
+    "tracks": []
   },
   "5439": {
     "album": "ABBA Gold"
@@ -28,17 +28,27 @@ var collection = {
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 // Update records function
 function updateRecords(id, prop, value) {
-  if(prop === "tracks"){
-    if(!collection[id].tracks){
-      var arr = [];
-      collection[id].tracks = arr;
-    }
-    collection[id][prop].push(value);
+  // If value is empty,
+  if (value === "") {
+    // ...delete the property
+    delete collection[id][prop];
   } else {
-    collection[id][prop] = value;
+    // If the property is tracks
+    if (prop === "tracks") {
+      // ...and there is no track property,
+      if (!collection[id].tracks) {
+        // ...create an array for the property
+        collection[id].tracks = [];
+      }
+      // ...add the track to the end of the property's array
+      collection[id][prop].push(value);
+      // If the property is not tracks
+    } else {
+      // ... add the value to the property
+      collection[id][prop] = value;
+    }
   }
-  
-return collection;
+  return collection;
 }
 // Examples
 updateRecords(5439, "artist", "ABBA"); // artist should be "ABBA"
